685ae344f8b2177d896dcbc96c355561e21c51f1
ediaz23 <Soxners$23>
2020-03-07 08:50:59 -0700

    sortOfSortedTest.java

---
+++ sortOfSortedTest.java
@@ -0,0 +1,45 @@
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class sortOfSortedTest {
+
+
+    @Test
+    public void testSortofSorted1(){
+        int[] A = {2,1,4,3,2,5,1};
+        int[] res = {}
+        assertEquals(res, sortOfSorted(A));
+    }
+
+    @Test
+    public void testSortofSorted2(){
+        int[] A = {2,1,4,3,2,5,1};
+        int[] res = {};
+        assertEquals(res, sortOfSorted(A));
+    }
+
+    @Test
+    public void testSortofSorted3(){
+        int[] A = {2,1,4,3,2,5,1};
+        int[] res = {};
+        assertEquals(res, sortOfSorted(A));
+    }
+
+    @Test
+    public void testSortofSorted4(){
+        int[] A = {2,1,4,3,2,5,1};
+        int[] res = {};
+        assertEquals(res, sortOfSorted(A));
+    }
+
+    @Test
+    public void testSortofSorted5(){
+        int[] A = {2,1,4,3,2,5,1};
+        int[] res = {};
+        assertEquals(res, sortOfSorted(A));
+    }
+
+
+}
